08c8bbb4455a858e4d7d995b6143eea68dfd0a23
felgonzalezh <felgonzalezh@gmail.com>
2020-02-27 16:03:28 -0500

    importing libraries

---
+++ Kaggle/Titanic_Project/Titanic.py
@@ -0,0 +1,8 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt # this is used for the plot the graph 
+import seaborn as sns # used for plot interactive graph.
+
+import matplotlib.pyplot as plt
+
+#%matplotlib inline
